37865104cf5088dbf66293bafa770ebd3ca662b6
Gamshikk <glebovich157rtyp@gmail.com>
2025-08-04 22:39:40 +0300

    Created exception middleware

---
+++ backend/backend/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using Application.Classes.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace backend.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                if (ex is OperationCanceledException) return;
+
+                Console.WriteLine(ex.Message);
+
+                await _handleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task _handleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+
+            var response = context.Response;
+
+            response.StatusCode = exception switch
+            {
+                BadRequestException => (int)HttpStatusCode.BadRequest,
+                NotFoundException => (int)HttpStatusCode.NotFound,
+                ArgumentException => (int)HttpStatusCode.BadRequest,
+                InternalServerException => (int)HttpStatusCode.InternalServerError,
+                _ => (int)HttpStatusCode.InternalServerError,
+            };
+
+            var errorResponse = new
+            {
+                message = response.StatusCode == (int)HttpStatusCode.InternalServerError 
+                    ? "Internal server error" 
+                    : exception.Message,
+                statusCode = response.StatusCode
+            };
+
+            var result = JsonSerializer.Serialize(errorResponse);
+
+            await context.Response.WriteAsync(result);
+        }
+    }
+
+}
