c290bedae6aecf88611e62327b8f721022cdd287
Gamshikk <glebovich157rtyp@gmail.com>
2025-08-04 22:38:41 +0300

    Created exception entities

---
+++ backend/Application/Classes/Exceptions/InternalServerException.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Classes.Exceptions
+{
+    public class InternalServerException : Exception
+    {
+        public InternalServerException(string message) : base(message) { }
+    }
+}
